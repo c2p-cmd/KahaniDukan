02a1504e9b0f710fe9b5be90a96b4c84f86b7666
Sharan Thakur <sharandt19@live.com>
2025-03-09 17:39:42 +0100

    refactor Story class to use record for improved readability and simplicity

--- src/main/java/org/thakur/kahanidukan/models/Story.java
+++ src/main/java/org/thakur/kahanidukan/models/Story.java
@@ -1,48 +1,7 @@
 package org.thakur.kahanidukan.models;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
-
 @Document(collection = "stories")
-public class Story {
-    @Id
-    private final String _id;
-    private final String title, author, story, moral;
-    private final String datetime;
-
-    public Story(String _id, String title, String author, String story, String moral, String datetime) {
-        this._id = _id;
-        this.title = title;
-        this.author = author;
-        this.story = story;
-        this.moral = moral;
-        this.datetime = datetime;
-    }
-
-    public String get_id() {
-        return _id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getStory() {
-        return story;
-    }
-
-    public String getMoral() {
-        return moral;
-    }
-
-    public LocalDateTime getDuration() {
-        return ZonedDateTime.parse(datetime).toLocalDateTime();
-    }
+public record Story(String id, String datetime, String title, String content, String moral, String author) {
 }
