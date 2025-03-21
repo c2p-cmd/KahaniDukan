554a58bcd787b30e4c86e5bfdf568c45e02af2e6
Sharan Thakur <sharandt19@live.com>
2025-03-21 13:35:18 +0100

    Add EmptyStoryException to handle empty story submissions

---
+++ src/main/java/org/thakur/kahanidukan/models/EmptyStoryException.java
@@ -0,0 +1,8 @@
+package org.thakur.kahanidukan.models;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Story is empty")
+public class EmptyStoryException extends RuntimeException {
+}
